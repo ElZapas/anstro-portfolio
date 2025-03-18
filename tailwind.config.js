d4973100a4a36a953f493b4e5c0ac0d8494e2c17
Anstro <anstrosolano@gmail.com>
2025-03-17 21:11:12 -0500

    Configurarion Tailwind para Satoshi

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'satoshi-light': ['Satoshi-Light', 'sans-serif'],
+        'satoshi-light-italic': ['Satoshi-LightItalic', 'sans-serif'],
+        'satoshi-regular': ['Satoshi-Regular', 'sans-serif'],
+        'satoshi-italic': ['Satoshi-Italic', 'sans-serif'],
+        'satoshi-medium': ['Satoshi-Medium', 'sans-serif'],
+        'satoshi-medium-italic': ['Satoshi-MediumItalic', 'sans-serif'],
+        'satoshi-bold': ['Satoshi-Bold', 'sans-serif'],
+        'satoshi-bold-italic': ['Satoshi-BoldItalic', 'sans-serif'],
+        'satoshi-black': ['Satoshi-Black', 'sans-serif'],
+        'satoshi-black-italic': ['Satoshi-BlackItalic', 'sans-serif'],
+        'satoshi-variable': ['Satoshi-Variable', 'sans-serif'],
+        'satoshi-variable-italic': ['Satoshi-VariableItalic', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
