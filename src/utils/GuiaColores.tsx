e2ed6f415bbd699d86e14ee5978f736a4920e2fa
Anstro <anstrosolano@gmail.com>
2025-03-29 22:36:43 -0500

    Agregando guia de colores

---
+++ src/utils/GuiaColores.tsx
@@ -0,0 +1,25 @@
+/**
+ * @abstract Guia de colores
+ * @description Guia de colores para el proyecto
+ * @file GuiaColores.tsx
+ * @example
+ * import GuiaColores from './utils/GuiaColores'
+ * @returns {React.FunctionComponent}
+ * @since 2021-08-29
+ */
+
+export default function GuiaColores() {
+  return (
+    <>
+    <div>GuiaColores</div>
+    <div className='bg-hero-principal'></div>
+    <div className='bg-fondo-principal'></div>
+    <div className='bg-fondo-secundario'></div>
+    <div className='bg-hero-principal'></div>
+    <div className='bg-acento'></div>
+    <div className='bg-texto-principal'></div>
+    <div className='bg-texto-secundario'></div>
+    <div className='bg-card'></div>
+    </>
+  )
+}
